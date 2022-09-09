d6f7541768e7e997d62ddab05f42de2d0c02e030
R RANJITH KUMAR <79324564+RajaRanjithkumar17@users.noreply.github.com>
2022-09-09 19:27:06 +0530

    Add files via upload

---
+++ project/business-page/script.js
@@ -0,0 +1,10 @@
+var na="ranjith";
+var vowel=["a","e","i","o","u"];
+var count=0
+for(i=0;i<na.length;i++){
+    if
+       ( vowel.includes(na[i])){
+        count=count+1
+    }
+    console.log(count)
+}
